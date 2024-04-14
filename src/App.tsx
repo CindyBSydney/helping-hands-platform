6ec2ab309a912cdab358778ee1b31a87f888d579
CindyBSydney <cindy.bosibori@strathmore.edu>
2024-04-14 15:48:43 +0300

    feat: add main layout components into App
    The Navbar, Header, AboutSection, Form, and Footer components were added and wrapped within a custom Theme provider to form the basic structure of the landing page.

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,67 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Button from '@mui/material/Button';
+import Theme from './theme/theme';
+//import Box from '@mui/material/Box';
+import Centered from './components/Centered';
+//import TheButton from './components/TheButton';
+import Contact from './pages/Contact';
+import Alert from '@mui/material/Alert';
+
+
+//Landing page
+import Navbar from './components/Navbar';
+import AboutSection from './components/AboutSection';
+import Header from './components/Header';
+import Form from './components/Form';
+import Footer from './components/Footer';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+  // <Theme>
+  //   {/* 
+  //   <Box
+  //       sx={{ 
+  //         display: 'flex', //flexbox
+  //         justifyContent: 'center', //vertical 
+  //         alignItems: 'center', //horizontal
+  //         height: '100vh',
+  //       }} 
+  //   >
+  //   */}
+  //   <Centered>
+  //     <Button 
+  //       variant="contained"
+  //       sx={{ 
+  //         marginTop: 2, //8px is the default value of the margin-top
+  //         backgroundColor: 'red',
+  //       }}
+  //       >
+  //         Hello world
+  //     </Button>
+  //   </Centered>
+  //   {/*</Box>*/}
+
+  //   {/* 
+  //   <TheButton> 
+  //     Hello world 
+  //   </TheButton>
+  //   */}
+
+
+  //   <Contact />
+  // </Theme>
+
+  <Theme>
+      <Navbar />
+      <Header />
+      <AboutSection />
+      <Form />
+      <Footer />
+
+  </Theme>
+
+
   );
 }
 
