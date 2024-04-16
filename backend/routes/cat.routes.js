d389f07a0b859b6114fd249ac31e6f0f4c528b5b
CindyBSydney <cindy.bosibori@strathmore.edu>
2024-04-16 22:14:59 +0300

    refactor: change get method to post
    Closes #1

--- backend/routes/cat.routes.js
+++ backend/routes/cat.routes.js
@@ -3,7 +3,7 @@ const router = express.Router();
 const catController = require('../controller/cat.controller');
 
 //add a cat to the array
-router.get('/add/:name', catController.create); 
+router.post('/add/:name', catController.create); 
 // /add is the path that the server should respond to
 // :name is the parameter that the server should extract from the URL
 // catController.create is the function that should be called when the server receives a request to this path
