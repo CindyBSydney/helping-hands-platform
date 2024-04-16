b20027355f86ac26d4e771fa0987bf6aa08d8f58
CindyBSydney <cindy.bosibori@strathmore.edu>
2024-04-16 11:42:37 +0300

    feat: add create function to add new cat names
    See #1

---
+++ backend/controller/cat.controller.js
@@ -0,0 +1,21 @@
+
+const cat = []; //array to store the cats
+
+//function to create a new cat and add it to the array of cats, then send a response to the client
+exports.create = (req, res) => { 
+    //function to create a new cat and add it to the array of cats, then send a response to the client
+    //exports means that the function is available to other files
+    //create means that the function is responsible for creating a new cat
+    //req is the request object that contains information about the HTTP request
+    //res is the response object that contains methods for sending a response to the client
+
+    const { name } = req.params; //extract the name parameter from the request
+    cat.push(name); //add the name to the array of cats
+    //cats.push(req.params.name);
+    res.send("Cat created: " + name); //send a response to the client with the message "Cat created" 
+};
+
+
+
+
+
